fd678c121e5789bc499ad8717589c2b66bca37e7
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2022-03-12 17:37:08 +0000

    Created tailscale vpn docker image

--- Docker/vpn/entrypoint.sh
+++ Docker/vpn/entrypoint.sh
@@ -1,8 +1,5 @@
-echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf
-sysctl -p /etc/sysctl.conf
+tailscale up \
+--auth-key ${AUTH-KEY} \
+--advertise-routes ${ADVERTISE-ROUTES}
 
-tailscaled &
-tailscale up --auth-key=tskey-kGzQnr2CNTRL-FeGnxeYLNabBbwWHaH6ZAR --advertise-routes "10.10.0.0/24"
-
-wait
+tailscaled
