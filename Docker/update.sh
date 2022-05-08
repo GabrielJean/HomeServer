d4b8860968b02c9ffec4f76139ca973bff44de4f
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2022-05-07 20:39:54 -0400

    updated compose update script

--- Docker/update.sh
+++ Docker/update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for d in ./*/ ; do (cd "$d" && docker-compose pull && docker-compose up --force-recreate --build -d); done
+for d in ./*/ ; do (cd "$d" && docker compose pull && docker compose up --force-recreate --build -d); done
