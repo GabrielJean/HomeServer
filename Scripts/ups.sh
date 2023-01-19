5c14e11ed0c475d0a273a43e6e626472240034bd
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2023-01-18 20:54:41 -0500

    updated ups.sh with localhost

--- Scripts/ups.sh
+++ Scripts/ups.sh
@@ -4,7 +4,7 @@ State=$(sudo pwrstat -status | grep State | sed 's/State........................
 
 if [ $State == "Normal" ]
 then
-    curl -fsS -m 10 --retry 5 -o /dev/null http://10.10.0.8:9500/ping/3571ae4b-2d0f-4484-9e6e-d53fdb8cb4b6
+    curl -fsS -m 10 --retry 5 -o /dev/null http://localhost:9500/ping/3571ae4b-2d0f-4484-9e6e-d53fdb8cb4b6
 else
-    curl -fsS -m 10 --retry 5 -o /dev/null http://10.10.0.8:9500/ping/3571ae4b-2d0f-4484-9e6e-d53fdb8cb4b6/fail
+    curl -fsS -m 10 --retry 5 -o /dev/null http://localhost:9500/ping/3571ae4b-2d0f-4484-9e6e-d53fdb8cb4b6/fail
 fi
